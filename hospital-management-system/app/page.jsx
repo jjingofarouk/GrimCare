349d55bcbd3ed5bdf20e7fce50fae18074e2870a
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-27 12:58:22 +0300

    Update page.jsx

---
+++ hospital-management-system/app/page.jsx
@@ -0,0 +1,17 @@
+import Header from './Header';
+import Sidebar from './Sidebar';
+import DashboardOverview from './dashboard/DashboardOverview';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1">
+        <Header />
+        <main className="p-6">
+          <DashboardOverview />
+        </main>
+      </div>
+    </div>
+  );
+}
