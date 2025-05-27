bf5d651bf27ea271c2fa46b75a392295fa037efd
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-27 23:36:37 +0300

    Update AppointmentForm.jsx

--- hospital-management-system/app/appointment/AppointmentForm.jsx
+++ hospital-management-system/app/appointment/AppointmentForm.jsx
@@ -1,5 +1,6 @@
 "use client" ;
 
+import React from 'react';
 import { useState } from 'react';
 import styles from './AppointmentForm.module.css';
 import { createAppointment } from './appointmentService';
