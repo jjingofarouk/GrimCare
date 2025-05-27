2d6b04a968f2d3eb7bd136e6eaeae7026e554429
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-27 23:42:53 +0300

    Update page.jsx

--- hospital-management-system/app/auth/page.jsx
+++ hospital-management-system/app/auth/page.jsx
@@ -9,7 +9,7 @@ export default function AuthPage() {
       <h2 className="text-2xl font-bold mb-4 text-center">Welcome</h2>
       <LoginForm />
       <p className="text-center mt-4">
-        Don't have an account? <a href="/auth/register" className="text-blue-500">Register</a>
+        Don&apos;t have an account? <a href="/auth/register" className="text-blue-500">Register</a>
       </p>
     </div>
   );
