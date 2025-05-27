a74412a166b7fdf0df088dcbfe2c2b68b7007648
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-27 21:11:47 +0300

    Update Header.jsx

--- hospital-management-system/app/Header.jsx
+++ hospital-management-system/app/Header.jsx
@@ -1,6 +1,6 @@
-"use client";
+'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { UserCircleIcon, BellIcon, Bars3Icon } from '@heroicons/react/24/outline';
 import styles from './Header.module.css';
 import Link from 'next/link';
