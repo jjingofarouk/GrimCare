2b1dff31d2ef9bf6aec958fe7164038e363ac8cd
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-27 13:22:27 +0300

    Update layout.jsx

---
+++ hospital-management-system/app/accounting/layout.jsx
@@ -0,0 +1,14 @@
+import Header from '../Header';
+import Sidebar from '../Sidebar';
+
+export default function AccountingLayout({ children }) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1">
+        <Header />
+        <main className="p-6">{children}</main>
+      </div>
+    </div>
+  );
+}
