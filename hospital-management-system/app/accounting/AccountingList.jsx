f028211ee70ef2cc05aadfaa2f73eadaba77a005
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-27 21:16:38 +0300

    Update AccountingList.jsx

--- hospital-management-system/app/accounting/AccountingList.jsx
+++ hospital-management-system/app/accounting/AccountingList.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from 'react';
 import { useState, useEffect } from 'react';
 import AccountingCard from './AccountingCard';
 import styles from './AccountingList.module.css';
