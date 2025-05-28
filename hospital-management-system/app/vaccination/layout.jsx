e4df3a3237a2790b5321320a950816336c8db6bb
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:55:59 +0300

    Update layout.jsx

--- hospital-management-system/app/vaccination/layout.jsx
+++ hospital-management-system/app/vaccination/layout.jsx
@@ -1,13 +1,13 @@
 // vaccination/layout.jsx
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+
+
 
 const VaccinationLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
+
+
       <main>{children}</main>
     </div>
   );
