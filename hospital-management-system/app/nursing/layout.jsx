d2512eadda66e039ccb7ef2137bdf550a0a50e03
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:49:13 +0300

    Update layout.jsx

--- hospital-management-system/app/nursing/layout.jsx
+++ hospital-management-system/app/nursing/layout.jsx
@@ -1,13 +1,13 @@
 // nursing/layout.jsx
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+
+
 
 const NursingLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
+
+
       <main>{children}</main>
     </div>
   );
