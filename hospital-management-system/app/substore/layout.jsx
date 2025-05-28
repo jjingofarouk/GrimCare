db42977b43974ca1e8542e22e574fa24cbd29a96
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:54:23 +0300

    Update layout.jsx

--- hospital-management-system/app/substore/layout.jsx
+++ hospital-management-system/app/substore/layout.jsx
@@ -1,13 +1,12 @@
 // substore/layout.jsx
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+
+
 
 const SubstoreLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
+
       <main>{children}</main>
     </div>
   );
