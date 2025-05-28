12cabf6e64be0bceb93a6c0c5d766e8b9e8b7572
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:55:31 +0300

    Update layout.jsx

--- hospital-management-system/app/utilities/layout.jsx
+++ hospital-management-system/app/utilities/layout.jsx
@@ -1,13 +1,13 @@
 // utilities/layout.jsx
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+
+
 
 const UtilitiesLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
+
+
       <main>{children}</main>
     </div>
   );
