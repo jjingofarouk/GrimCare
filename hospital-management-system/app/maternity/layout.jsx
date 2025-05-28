df8e4647d75209be6eafae0273b05f984a363615
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:46:24 +0300

    Update layout.jsx

--- hospital-management-system/app/maternity/layout.jsx
+++ hospital-management-system/app/maternity/layout.jsx
@@ -1,13 +1,13 @@
 // maternity/layout.jsx
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+
+
 
 const MaternityLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
+
+
       <main>{children}</main>
     </div>
   );
