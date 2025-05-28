a2c5bc32c2e703b10319207aea1174f47d389a2b
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:44:52 +0300

    Update layout.jsx

--- hospital-management-system/app/helpdesk/layout.jsx
+++ hospital-management-system/app/helpdesk/layout.jsx
@@ -1,13 +1,13 @@
 // helpdesk/layout.jsx
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+
+
 
 const HelpdeskLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
+
+      
       <main>{children}</main>
     </div>
   );
