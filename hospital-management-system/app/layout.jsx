61f70a333a5b9167360cada7b796c84791594e06
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 19:36:50 +0300

    Update layout.jsx

--- hospital-management-system/app/layout.jsx
+++ hospital-management-system/app/layout.jsx
@@ -1,11 +1,5 @@
 // app/layout.tsx
 import './globals.css';
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Your App Title',
-  description: 'Your app description here',
-};
 
 export default function RootLayout({
   children,
