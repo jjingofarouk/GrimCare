f51f4bd6eaefbfd9c534b3f6f1c1b2bce34788cc
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:49:44 +0300

    Update layout.jsx

--- hospital-management-system/app/operation-theatre/layout.jsx
+++ hospital-management-system/app/operation-theatre/layout.jsx
@@ -1,13 +1,13 @@
 // operation-theatre/layout.jsx
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+
+
 
 const OperationTheatreLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
+
+
       <main>{children}</main>
     </div>
   );
