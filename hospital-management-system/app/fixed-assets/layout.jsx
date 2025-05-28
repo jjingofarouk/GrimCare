17331a9b2750eadf81314b4b5b48be134f4d0f07
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:44:14 +0300

    Update layout.jsx

--- hospital-management-system/app/fixed-assets/layout.jsx
+++ hospital-management-system/app/fixed-assets/layout.jsx
@@ -1,13 +1,13 @@
 // fixed-assets/layout.jsx
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+
+
 
 const FixedAssetsLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
+
+
       <main>{children}</main>
     </div>
   );
