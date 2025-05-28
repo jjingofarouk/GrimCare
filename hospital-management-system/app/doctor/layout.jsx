8b0304961439ee481e291a3107b964ebafaa988f
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 22:39:41 +0300

    Update layout.jsx

--- hospital-management-system/app/doctor/layout.jsx
+++ hospital-management-system/app/doctor/layout.jsx
@@ -1,9 +1,8 @@
-'use client';
-
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Container, Box } from '@mui/material';
 
-const DoctorLayout = ({ children }) => {
+const DoctorLayout = () => {
   return (
     <Box sx={{ minHeight: '100vh', bgcolor: 'grey.100' }}>
       <AppBar position="static" color="primary">
@@ -14,7 +13,7 @@ const DoctorLayout = ({ children }) => {
         </Toolbar>
       </AppBar>
       <Container sx={{ mt: 4, p: 2 }}>
-        {children}
+        <Outlet />
       </Container>
     </Box>
   );
