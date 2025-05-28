236ac057349977889aecfc97f5b2d48e32eea2b1
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 20:20:32 +0300

    Update layout.jsx

--- hospital-management-system/app/layout.jsx
+++ hospital-management-system/app/layout.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-
-
-// app/layout.jsx
 import './globals.css';
+import Header from './Header';
+import Sidebar from './Sidebar';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="antialiased bg-white text-black">
-        {children}
+      <body className="antialiased bg-white text-black flex">
+        {/* Sidebar stays on the left */}
+        <Sidebar />
+
+        <div className="flex flex-col flex-1">
+          {/* Header stays on top */}
+          <Header />
+
+          {/* Main content goes here */}
+          <main className="flex-1">
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   );
