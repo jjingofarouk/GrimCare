cefeb3d0b8793fccbe4a5d67799ed00bb2a8e670
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:45:23 +0300

    Update layout.jsx

--- hospital-management-system/app/incentive/layout.jsx
+++ hospital-management-system/app/incentive/layout.jsx
@@ -1,13 +1,12 @@
 // incentive/layout.jsx
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+
 
 const IncentiveLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
+
+
       <main>{children}</main>
     </div>
   );
