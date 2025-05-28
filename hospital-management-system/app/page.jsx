b5812c6e375d7c2da516f9c9da25b667aae539b5
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 19:44:01 +0300

    Update page.jsx

--- hospital-management-system/app/page.jsx
+++ hospital-management-system/app/page.jsx
@@ -1,7 +1,5 @@
 
 import React from 'react';
-import Header from './Header';
-import Sidebar from './Sidebar';
 import DashboardOverview from './dashboard/DashboardOverview';
 
 export default function Home() {
