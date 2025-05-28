d67932f29c7731bdc68f2c7c2b8bd9d942977bd4
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:55:01 +0300

    Update layout.jsx

--- hospital-management-system/app/system-admin/layout.jsx
+++ hospital-management-system/app/system-admin/layout.jsx
@@ -1,13 +1,13 @@
 // system-admin/layout.jsx
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+
+
 
 const SystemAdminLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
+
+
       <main>{children}</main>
     </div>
   );
