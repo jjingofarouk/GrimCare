cac5b56848764f036fd0ff6578989b8319b53b54
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:48:45 +0300

    Update layout.jsx

--- hospital-management-system/app/nhif/layout.jsx
+++ hospital-management-system/app/nhif/layout.jsx
@@ -1,13 +1,13 @@
 // nhif/layout.jsx
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+
+
 
 const NhifLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
+
+
       <main>{children}</main>
     </div>
   );
