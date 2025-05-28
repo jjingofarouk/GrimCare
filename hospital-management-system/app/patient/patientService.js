729ebfdc12bbc206972f6516885b3aa27aeaf9a5
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 15:09:37 +0300

    Update patientService.js

--- hospital-management-system/app/patient/patientService.js
+++ hospital-management-system/app/patient/patientService.js
@@ -1,12 +1,36 @@
-// patient/patientService.js
 import api from '../api';
 
 export const getPatients = async () => {
-  const response = await api.get('/api/patient');
+  const response = await api.get('/api/patients');
+  return response.data;
+};
+
+export const getPatientById = async (id) => {
+  const response = await api.get(`/api/patients/${id}`);
   return response.data;
 };
 
 export const createPatient = async (data) => {
-  const response = await api.post('/api/patient', data);
+  const response = await api.post('/api/patients', data);
+  return response.data;
+};
+
+export const updatePatient = async (id, data) => {
+  const response = await api.put(`/api/patients/${id}`, data);
+  return response.data;
+};
+
+export const deletePatient = async (id) => {
+  const response = await api.delete(`/api/patients/${id}`);
+  return response.data;
+};
+
+export const searchPatients = async (query) => {
+  const response = await api.get('/api/patients/search', { params: { query } });
+  return response.data;
+};
+
+export const getPatientHistory = async (patientId) => {
+  const response = await api.get(`/api/patients/${patientId}/history`);
   return response.data;
 };
