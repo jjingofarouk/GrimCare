4fa9e0bb63b8b77317a1f6f40c6ff017a30b9332
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:53:22 +0300

    Update layout.jsx

--- hospital-management-system/app/settings/layout.jsx
+++ hospital-management-system/app/settings/layout.jsx
@@ -1,13 +1,11 @@
 // settings/layout.jsx
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+
+
 
 const SettingsLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
       <main>{children}</main>
     </div>
   );
