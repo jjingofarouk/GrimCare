328a9fa32f9830299becd2992b884d634bae95f9
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:50:49 +0300

    Update layout.jsx

--- hospital-management-system/app/pharmacy/layout.jsx
+++ hospital-management-system/app/pharmacy/layout.jsx
@@ -1,13 +1,13 @@
 // pharmacy/layout.jsx
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+
+
 
 const PharmacyLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
+
+
       <main>{children}</main>
     </div>
   );
