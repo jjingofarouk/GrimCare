be8a6cd6b21c9f5eb367d3dd61acf9a166e94ee9
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:40:43 +0300

    Update layout.jsx

--- hospital-management-system/app/accounting/layout.jsx
+++ hospital-management-system/app/accounting/layout.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Header from '../Header';
-import Sidebar from '../Sidebar';
+
+
 
 export default function AccountingLayout({ children }) {
   return (
     <div className="flex min-h-screen">
-      <Sidebar />
+
       <div className="flex-1">
-        <Header />
+
         <main className="p-6">{children}</main>
       </div>
     </div>
