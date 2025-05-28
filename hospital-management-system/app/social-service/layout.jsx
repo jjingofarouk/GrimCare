5996f01de6fbcb13c5c830405b7f4e520f3855a1
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-28 12:53:52 +0300

    Update layout.jsx

--- hospital-management-system/app/social-service/layout.jsx
+++ hospital-management-system/app/social-service/layout.jsx
@@ -1,13 +1,13 @@
 // social-service/layout.jsx
 import React from 'react';
-import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+
+
 
 const SocialServiceLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
+
+
       <main>{children}</main>
     </div>
   );
