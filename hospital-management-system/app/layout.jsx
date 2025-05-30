fa61620a4c0c6828064162b90ff2dff7e988c074
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-31 00:45:07 +0300

    Update layout.jsx

--- hospital-management-system/app/layout.jsx
+++ hospital-management-system/app/layout.jsx
@@ -4,19 +4,33 @@ import React, { useState } from 'react';
 import './globals.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import { useAuth } from './useAuth';
 
 export default function RootLayout({ children }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const { user, loading } = useAuth();
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
   return (
-    <div className="layout-container">
-      <Header toggleSidebar={toggleSidebar} />
-      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <main className="main-content">{children}</main>
-    </div>
+    <html lang="en">
+      <body>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <div className="layout-container">
+            {user && (
+              <>
+                <Header toggleSidebar={toggleSidebar} />
+                <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+              </>
+            )}
+            <main className="main-content">{children}</main>
+          </div>
+        )}
+      </body>
+    </html>
   );
 }
