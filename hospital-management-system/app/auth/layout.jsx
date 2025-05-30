5097566f193ee97a17bea940dd215e7547d42375
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-31 00:44:32 +0300

    Update layout.jsx

--- hospital-management-system/app/auth/layout.jsx
+++ hospital-management-system/app/auth/layout.jsx
@@ -1,15 +1,12 @@
-"use client";
+'use client';
 
 import React from 'react';
-
 import '../globals.css';
 
 export default function AuthLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <main>{children}</main>
-      </body>
-    </html>
+    <div>
+      {children}
+    </div>
   );
 }
