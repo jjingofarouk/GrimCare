12845e394e89d7d57c9cd0fb80e43ac4feec9742
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-31 05:55:24 +0300

    Update LoginForm.jsx

--- hospital-management-system/app/auth/LoginForm.jsx
+++ hospital-management-system/app/auth/LoginForm.jsx
@@ -1,7 +1,8 @@
-'use client';
+"use client";
 
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link'; // Add this import
 import styles from './LoginForm.module.css';
 import { login } from './authService';
 
