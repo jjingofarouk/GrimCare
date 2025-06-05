457445554a081d95c96bc2b7766fbbb8529b6aac
Jjingo Farouk <jjingofarouq@gmail.com>
2025-06-05 03:46:18 -0700

    Update layout.jsx

--- carewave/app/doctor/layout.jsx
+++ carewave/app/doctor/layout.jsx
@@ -1,21 +1,12 @@
 "use client";
 import React from 'react';
-import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import { Box } from '@mui/material';
 import styles from './layout.module.css';
 
 const DoctorLayout = ({ children }) => {
   return (
     <Box className={styles.container}>
-      <AppBar position="static" color="primary" className={styles.appBar}>
-        <Toolbar className={styles.toolbar}>
-          <Typography variant="h6" className={styles.title}>
-            Doctor Management - Uganda Health System
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Box className={styles.content}>
-        {children}
-      </Box>
+      {children}
     </Box>
   );
 };
